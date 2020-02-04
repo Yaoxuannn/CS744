dfe1958ff0a28e3533a6972535f2a71f82fdfd24
Wei Yaoxuan <justin13wyx@gmail.com>
2020-02-03 20:56:45 -0600

    用户表模型建立完成, 登录功能实现, RPC实现

---
+++ api/api.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, jsonify
+from flasgger import Swagger
+from nameko.standalone.rpc import ClusterRpcProxy
+
+
+app = Flask(__name__)
+Swagger(app)
+CONFIG = {'AMQP_URI': "amqp://guest:guest@localhost"}
+
+
+@app.route("/api/v1/login", methods=['POST'])
+def login():
+    if not request.json:
+        return pack_response(10002, "请求参数异常")
+    username = request.json['username']
+    password = request.json['password']
+    with ClusterRpcProxy(CONFIG) as rpc:
+        status, message = rpc.user_service.user_login(username, password)
+    return pack_response(status, message)
+
+
+def check_params(params, essentials):
+    for k, v in params.items():
+        if k in essentials and not v:
+            return False
+    return True
+
+
+def pack_response(status_code=20000, msg="ok", **kwargs):
+    data = {
+        "status": status_code,
+        "msg": msg
+    }
+    for k, v in kwargs.items():
+        data.update({k: v})
+    res = jsonify(data)
+    res.headers['Access-Control-Allow-Origin'] = "*"
+    res.headers['X-XSS-Protection'] = "1"
+    return res
+
+
+def clean_params(params):
+    _params = params.copy()
+    for k, v in params.items():
+        if not v:
+            _params.pop(k)
+    return _params
+
+
+@app.before_request
+def options_handler():
+    if request.method == "OPTIONS":
+        res = pack_response()
+        res.headers['Access-Control-Allow-Methods'] = "GET, POST"
+        res.headers['Access-Control-Allow-Headers'] = 'content-type'
+        res.headers['Access-Control-Allow-Credentials'] = "true"
+        res.headers['Access-Control-Max-Age'] = "1728000"
+        return res
+    else:
+        pass
+
+
+app.run(debug=True)
---
+++ service/user.py
@@ -0,0 +1,63 @@
+from hashlib import md5
+from nameko.rpc import rpc
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
+
+
+Base = declarative_base()
+engine = create_engine('sqlite:///D:\\OneDrive - University of Wisconsin-La Crosse\\CS 744\\Code\\CS744\\user.db')
+Session = sessionmaker()
+Session.configure(bind=engine)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_name = Column(String, primary_key=True, unique=True, nullable=False)
+    user_fullname = Column(String)
+    user_email = Column(String)
+    user_phone = Column(String)
+    user_status = Column(String, default=0)
+    user_token = Column(String, unique=True)
+    user_type = Column(String, nullable=False)
+    addition1 = Column(String)
+    addition2 = Column(String)
+    preferred_info = Column(String, default="EMAIL")
+
+
+class UserSecret(Base):
+    __tablename__ = "user_secret"
+
+    user_name = Column(String, primary_key=True)
+    secret = Column(String, nullable=False)
+
+
+class UserService(object):
+    name = "user_service"
+
+    @staticmethod
+    def check_login_status():
+        pass
+
+    def login_code_validate(self, ):
+        pass
+
+    @rpc
+    def user_registry(self, ):
+        pass
+
+    @rpc
+    def user_login(self, username, password):
+        session = Session()
+        existed_user = session.query(User.user_name).filter(User.user_name == username).first()
+        if not existed_user:
+            return 10002, "用户不存在"
+        if session.query(UserSecret).filter(UserSecret.user_name == username, UserSecret.secret == password).first():
+            return 20000, "OK"
+        return 10001, "登录信息错误"
+
+    @rpc
+    def user_logout(self, ):
+        pass
