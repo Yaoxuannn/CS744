0fea7d1e85bab8d7b3732cd14f0adf0a450b7d5b
Wei Yaoxuan <justin13wyx@gmail.com>
2020-03-31 12:38:11 -0500

    数据库填充数据, 拉取待注册列表bug修复

--- api/api.py
+++ api/api.py
@@ -102,7 +102,7 @@ def get_register_list():
             for event in register_list:
                 user_info = rpc.user_service.get_user_info(event["target"])
                 if user_info['user_type'] == "patient":
-                    physician_id = user_info['associateID']
+                    physician_id = event['additional_info']
                 data.append({
                     "eventID": event['event_id'],
                     "userID": user_info['user_id'],
